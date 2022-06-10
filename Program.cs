998955afecafea70876d4817e80edec0df5e4ada
parohodov@hotmail.com <parohodov@hotmail.com>
2022-06-10 11:12:06 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace pornhub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pic = File.ReadAllBytes("1.jpg");
+            var text = Convert.ToBase64String(pic);
+
+            File.WriteAllText("1.txt", text);
+            text = File.ReadAllText("1.txt");
+            pic = Convert.FromBase64String(text);
+        }
+    }
+}
